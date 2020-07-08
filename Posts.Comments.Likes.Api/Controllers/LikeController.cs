665dd274b931e8a11be73d5857fe3ea617fdc8a0
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-07-08 22:00:49 +0300

    .

---
+++ Posts.Comments.Likes.Api/Controllers/LikeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Posts.Comments.Likes.Application.Commands;
+
+namespace Posts.Comments.Likes.Api.Controllers
+{
+    public class LikeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public LikeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("Like")]
+        public async Task<IActionResult> Like([FromBody]LikeCommand command)
+        {
+            var like = await _mediator.Send(command);
+
+            return Created($"Like/{like.Id}", like);
+        }
+    }
+}
---
+++ Posts.Comments.Likes.Api/Startup.cs
@@ -0,0 +1,64 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Posts.Comments.Likes.Api.Filters;
+using Posts.Comments.Likes.Application.Configurations;
+using Posts.Comments.Likes.Application.Validators;
+using Posts.Comments.Likes.Infrastructure;
+using Posts.Comments.Likes.Infrastructure.Configurations;
+using Posts.Comments.Likes.Infrastructure.Services;
+
+namespace Posts.Comments.Likes.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MongoConfiguration>(Configuration.GetSection(nameof(MongoConfiguration)));
+
+            services.AddSingleton<IMongoConfiguration>(o => o.GetRequiredService<IOptions<MongoConfiguration>>().Value);
+
+            services.AddSingleton<LikeDbContext>();
+
+            services.AddSingleton<ILikeService, LikeService>();
+
+            services.AddApplication();
+
+            services
+                .AddControllers(o => o.Filters.Add(typeof(GlobalExceptionFilter)))
+                .AddNewtonsoftJson(o => o.SerializerSettings.NullValueHandling = NullValueHandling.Ignore)
+                .AddFluentValidation(o => o.RegisterValidatorsFromAssemblyContaining<LikeValidator>());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
